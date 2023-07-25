22310e05e68d4aa334c707dcbed7c8f7abb00c47
DaniBonica001 <danielabc_0106@hotmail.com>
2023-07-25 16:32:54 -0500

    Context to handle the user and navigate to tasks after register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/tasks" element={<h1>tasks page</h1>}></Route>
-        <Route path="/add-task" element={<h1>new task page</h1>}></Route>
-        <Route path="/tasks/:id" element={<h1>update page</h1>}></Route>
-        <Route path="/profile" element={<h1>profile page</h1>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/tasks" element={<h1>tasks page</h1>}></Route>
+          <Route path="/add-task" element={<h1>new task page</h1>}></Route>
+          <Route path="/tasks/:id" element={<h1>update page</h1>}></Route>
+          <Route path="/profile" element={<h1>profile page</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 export default App;
