f59c6e603177668e3c453911dbd827613c217368
DaniBonica001 <danielabc_0106@hotmail.com>
2023-07-26 15:56:57 -0500

    basic components of other pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import { AuthProvider } from "./context/AuthContext";
+import TaskPage from "./pages/TaskPage";
+import TaskFormPage from "./pages/TaskFormPage";
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home page</h1>}></Route>
+          <Route path="/" element={<HomePage/>}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
-          <Route path="/tasks" element={<h1>tasks page</h1>}></Route>
-          <Route path="/add-task" element={<h1>new task page</h1>}></Route>
-          <Route path="/tasks/:id" element={<h1>update page</h1>}></Route>
-          <Route path="/profile" element={<h1>profile page</h1>}></Route>
+          <Route path="/tasks" element={<TaskPage/>}></Route>
+          <Route path="/add-task" element={<TaskFormPage/>}></Route>
+          <Route path="/tasks/:id" element={<TaskFormPage/>}></Route>
+          <Route path="/profile" element={<ProfilePage/>}></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
