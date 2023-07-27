e4cfe7a4a5bf3f85cb1cece41aaf3955caf24486
DaniBonica001 <danielabc_0106@hotmail.com>
2023-07-27 14:55:42 -0500

    styles modifications

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,36 +2,50 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 function NavBar() {
-  const { isAuthenticated, logout,user } = useAuth();
+  const { isAuthenticated, logout, user } = useAuth();
   return (
-    <nav
-      className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg"
-    >
-      <Link to="/tasks">
+    <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
+      <Link to={isAuthenticated ? "/tasks" : "/"}>
         <h1 className="text-2xl font-bold">Task Manager</h1>
       </Link>
       <ul className="flex gap-x-2">
         {isAuthenticated ? (
           <>
+            <li>Welcome {user.username}</li>
             <li>
-              Welcome {user.username}
+              <Link
+                to="/add-task"
+                className="bg-indigo-500 px-4 py-1 rounded-sm"
+              >
+                Add Task
+              </Link>
             </li>
             <li>
-              <Link to="/add-task" className="bg-indigo-500 px-4 py-1 rounded-sm">Add Task</Link>
-            </li>
-            <li>
-              <Link to="/" onClick={()=>{
-                logout()
-              }} className="bg-red-500  px-4 py-1 rounded-sm">Logout</Link>
+              <Link
+                to="/"
+                onClick={() => {
+                  logout();
+                }}
+                className="bg-red-500  px-4 py-1 rounded-sm"
+              >
+                Logout
+              </Link>
             </li>
           </>
         ) : (
           <>
             <li>
-              <Link to="/login" className="bg-indigo-500 px-4 py-1 rounded-sm">Login</Link>
+              <Link to="/login" className="bg-indigo-500 px-4 py-1 rounded-sm">
+                Login
+              </Link>
             </li>
             <li>
-              <Link to="/register" className="bg-indigo-500 px-4 py-1 rounded-sm">Register</Link>
+              <Link
+                to="/register"
+                className="bg-indigo-500 px-4 py-1 rounded-sm"
+              >
+                Register
+              </Link>
             </li>
           </>
         )}
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -3,6 +3,10 @@ import { useTask } from "../context/TaskContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
 
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+dayjs.extend(utc)
+
 function TaskFormPage() {
   const { register, handleSubmit, setValue } = useForm();
   const { createTask, getTask, updateTask } = useTask();
@@ -11,9 +15,15 @@ function TaskFormPage() {
 
   const onSubmit = (data) => {
     if (params.id) {
-      updateTask(params.id, data);
+      updateTask(params.id, {
+        ...data,
+        date:dayjs.utc(data.date).format(),
+      });
     } else {
-      createTask(data);
+      createTask({
+        ...data,
+        date:dayjs.utc(data.date).format(),
+      });
     }
     navigate("/tasks");
   };
@@ -34,6 +44,7 @@ function TaskFormPage() {
       <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md item">
         <h1 className="text-2xl font-bold">Add new task</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="title">Title</label>
           <input
             type="text"
             placeholder="Title"
@@ -41,14 +52,23 @@ function TaskFormPage() {
             autoFocus
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           />
+          <label htmlFor="
+          description">Description</label>
           <textarea
             rows="3"
             placeholder="Description"
             {...register("description")}
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           ></textarea>
+          <label htmlFor="date">Date</label>
+          <input 
+            type="date"
+            placeholder="Date"
+            {...register("date")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
 
-          <button>Save</button>
+          <button className="bg-indigo-500 px-3 py-2 rounded-md">Save</button>
         </form>
       </div>
     </div>
